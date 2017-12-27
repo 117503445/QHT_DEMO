e40ff99c8dab3ef8dc743f4aa5743d4eb9c8e772
117503445 <t117503445@gmail.com>
2017-12-27 18:58:46 +0800

    demo1227_1858

--- WPF_Window_Demo/MainWindow.xaml.cs
+++ WPF_Window_Demo/MainWindow.xaml.cs
@@ -10,19 +10,19 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace WPF_Window_Demo
 {
     /// <summary>
-    /// MainWindow.xaml 的交互逻辑
+    /// MainWIndow.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow : BaseWindow
+    public partial class MainWIndow : BaseWindow
     {
-        public MainWindow()
+        public MainWIndow()
         {
             InitializeComponent();
+            Console.WriteLine(i);
         }
     }
 }
