b57975ff53a61ab9c9f521f14288c674acc7c3ba
117503445 <t117503445@gmail.com>
2018-01-07 16:50:54 +0800

    demo0107_1650

---
+++ SettingDemo/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using s = SettingDemo.Properties.Settings;
+namespace SettingDemo
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Tb.Text = s.Default.b.ToString();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            s.Default.b = !s.Default.b;
+           Tb.Text= s.Default.b.ToString();
+        }
+    }
+}
