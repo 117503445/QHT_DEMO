325632f56af3873435ae33ab19944b7962db6efb
117503445 <t117503445@gmail.com>
2018-01-12 12:26:45 +0800

    demo0112_1226

---
+++ Open_PPT/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Open_PPT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process cmd = new Process();
+            cmd.StartInfo.FileName = @"C:\Program Files (x86)\Microsoft Office\root\Office16\POWERPNT.EXE";
+            cmd.StartInfo.Arguments = @"C:\Users\117503445\Desktop\1.ppt";
+            cmd.Start();
+
+        }
+    }
+}
