d05a94531c445fb5313b6760562b66ac69c27fa8
117503445 <t117503445@gmail.com>
2018-02-24 16:33:59 +0800

    demo0224_1633

---
+++ WindowsServiceDemo/Service1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsServiceDemo
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+        string filePath = @"D:\MyServiceLog.txt";
+        protected override void OnStart(string[] args)
+        {
+            File.AppendAllText(filePath,"OK"+DateTime.Now.ToString());
+        }
+
+        protected override void OnStop()
+        {
+            File.AppendAllText(filePath, "Stop" + DateTime.Now.ToString());
+        }
+    }
+}
