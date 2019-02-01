45cc7b932af0680a5115eb8a5f1aaa4b796bbe03
117503445 <t117503445@gmail.com>
2019-02-01 20:53:38 +0800

    0201-2053

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyStruct s = new MyStruct();
+            s.num++;
+            Console.WriteLine(s.num);
+        }
+    }
+    struct MyStruct
+    {
+        public double num;
+    }
+}
