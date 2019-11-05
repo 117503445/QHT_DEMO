a443fac239fc3a83771bfa5b0a7eb924bfe46233
117503445 <t117503445@gmail.com>
2019-11-05 22:22:53 +0800

    add WpfTest

---
+++ WpfTest/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnTest_Click(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine(Tv.IsVisible);
+            foreach (var item in Tv.Items)
+            {
+                //Debug.WriteLine(((UIElement)item).IsVisible);
+
+            }
+        }
+    }
+}
